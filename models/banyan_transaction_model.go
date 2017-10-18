0d0b07639cd9a5a03e39bd4af7b72dbe7fe7411d
Matthew Berryhill <matthewberryhill@gmail.com>
2017-10-17 18:08:01 -0600

    Implement BanyanTransaction model

---
+++ models/banyan_transaction_model.go
@@ -0,0 +1,24 @@
+package models
+
+import ()
+
+type BanyanTransaction struct {
+	Date				string
+	Num 				string
+	ShipperName			string
+	ShipperAddress		string
+	ShipperCity			string
+	ShipperState		string
+	ShipperZip			string
+	Customer			string
+	ShipToAddress1		string
+	ShipToAddress2		string
+	ShipToCity			string
+	ShipToState			string
+	ShipZip				string
+	PO 					string
+	Item				string
+	Qty 				string
+	UM					string
+	Class 				string
+}
