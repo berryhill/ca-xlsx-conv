ccd5c0a4c261c158161f258134a2cd10f1d301d5
Matthew Berryhill <matthewberryhill@gmail.com>
2017-10-17 18:11:39 -0600

    Implement QuickbooksTransaction model

---
+++ models/quickbooks_transaction_model.go
@@ -0,0 +1,19 @@
+package models
+
+import()
+
+type QuickbooksTransaction struct {
+	Date 				string
+	Num 				string
+	ShipToAddress1 		string
+	ShipToAddress2		string
+	ShipToCity 			string
+	ShipToState 		string
+	ShipZip				string
+	PO 					string
+	Item				string
+	Qty 				string
+	UM 					string
+	Class 				string
+
+}
